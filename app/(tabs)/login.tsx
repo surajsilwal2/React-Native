cad7f59a07aada3f1d3be9aedf2d06b49a20ede2
suraj <spro71612@gmail.com>
2025-11-03 22:02:13 +0545

    Update login component structure and correct screen name in layout

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,11 +1,11 @@
+import { View, Text } from 'react-native'
 import React from 'react'
-import { Text } from 'react-native'
 
 const login = () => {
   return (
-    <Text>
-        login
-    </Text>
+    <View>
+      <Text>login</Text>
+    </View>
   )
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
 import { Stack } from "expo-router";
-import { Text } from "react-native";
 
 export default function RootLayout() {
   return <>
 
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   </>;
 }
